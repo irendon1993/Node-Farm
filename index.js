8804ebb1023afbe026910d51659961d7f0dad5e2
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-15 20:02:02 -0500

    building API

--- index.js
+++ index.js
@@ -30,15 +30,19 @@ const url = require("url");
 
 ////////////////////////////////
 // Server
-const server = http.createServer((req, res) => {
-  console.log(req.url);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
 
+const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the Product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
