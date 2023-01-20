53acb595dc6f7ba3bc99fb8f5baf36c552d3b74a
Isaac Rendon <isaacrendon7@gmail.com>
2023-01-20 13:20:06 -0500

    Building a simple web server with http

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // File reading system module
 const fs = require('fs');
+const http = require('http');
+
+/////////////////////////////////////
+// Files
 
 // Blocking, synchronous way
 // Takes 2 arguments, path and character encoding
@@ -12,17 +16,29 @@ const fs = require('fs');
 // console.log("File Written!");
 
 //Non-blocking asynchronous way using callback with 2 data values the error(usually first) and the data variables
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!');
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written!');
-      });
-    });
-  });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written!');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+/////////////////////////////////////
+// Server
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end('Hello from the server!');
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
